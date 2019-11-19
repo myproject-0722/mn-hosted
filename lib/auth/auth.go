3c00079702abeffef3067d254273f58754dc2b04
ubuntu16.04.06 <lixu@yeah.net>
2019-11-19 11:10:26 +0800

    重写micro,添加API网关的JWT较验

---
+++ lib/auth/auth.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/micro/micro/plugin"
+	"github.com/myproject-0722/mn-hosted/lib/token"
+)
+
+// JWTAuthWrapper JWT鉴权Wrapper
+func JWTAuthWrapper(token *token.Token) plugin.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Println("auth plugin received: " + r.URL.Path)
+			// TODO 从配置中心动态获取白名单URL
+			if r.URL.Path == "/user/User/SignIn" || r.URL.Path == "/user/User/SignUp" || r.URL.Path == "/user/test" {
+				h.ServeHTTP(w, r)
+				return
+			}
+
+			tokenstr := r.Header.Get("Authorization")
+			userFromToken, e := token.Decode(tokenstr)
+
+			if e != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			log.Println("User Name : ", userFromToken.UserName)
+			r.Header.Set("X-Example-Username", userFromToken.UserName)
+			h.ServeHTTP(w, r)
+		})
+	}
+}
---
+++ micro/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/micro/micro/cmd"
+	"github.com/micro/micro/plugin"
+	"github.com/myproject-0722/mn-hosted/conf"
+	"github.com/myproject-0722/mn-hosted/lib/auth"
+	"github.com/myproject-0722/mn-hosted/lib/token"
+)
+
+//const name = "API gateway"
+func init() {
+	token := &token.Token{}
+	token.InitConfig(conf.ConsulAddresses, "micro", "config", "jwt-key", "key")
+
+	//plugin.Register(cors.NewPlugin())
+
+	plugin.Register(plugin.NewPlugin(
+		plugin.WithName("auth"),
+		plugin.WithHandler(
+			auth.JWTAuthWrapper(token),
+		),
+	))
+	/*
+		plugin.Register(plugin.NewPlugin(
+			plugin.WithName("tracer"),
+			plugin.WithHandler(
+				stdhttp.TracerWrapper,
+			),
+		))
+		plugin.Register(plugin.NewPlugin(
+			plugin.WithName("breaker"),
+			plugin.WithHandler(
+				hystrix.BreakerWrapper,
+			),
+		))
+		plugin.Register(plugin.NewPlugin(
+			plugin.WithName("metrics"),
+			plugin.WithHandler(
+				prometheus.MetricsWrapper,
+			),
+		))*/
+}
+
+func main() {
+	//stdhttp.SetSamplingFrequency(50)
+	/*t, io, err := tracer.NewTracer(name, "")
+	if err != nil {
+					log.Fatal(err)
+	}
+	defer io.Close()
+	opentracing.SetGlobalTracer(t)*/
+
+	/*hystrixStreamHandler := ph.NewStreamHandler()
+	hystrixStreamHandler.Start()
+	go http.ListenAndServe(net.JoinHostPort("", "8000"), hystrixStreamHandler)*/
+
+	cmd.Init()
+}
