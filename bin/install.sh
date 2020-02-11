d10cd58ee2f3b3e46e775cc837e5e384e4d5c031
lixu925 <lixu_11@yeah.net>
2020-02-11 14:29:20 +0800

    modify install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,10 @@
 go build ../rpcsvr/wallet/rpc-wallet-srv.go
 go build ../rpcsvr/order/rpc-order-srv.go
 go build ../rpcsvr/user/rpc-user-srv.go
-go build ../rpcsvr/node/rpc-node-srv.go
+go build -gcflags "-N -l" ../rpcsvr/node/rpc-node-srv.go
 go build ../apisvr/user/api-user-srv.go
 go build ../apisvr/node/api-node-srv.go
 go build ../apisvr/order/api-order-srv.go
-go build ../micro/
+#go build ../micro/
 go build -o time-srv ../timesrv/
 go build -gcflags "-N -l" ../notifysrv/alipay/alipay-notify.go 
